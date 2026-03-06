b098c54243f6e061f84dd18658086515a0f1da57
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-03-06 10:05:53 +0100

    1

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import themeStyles from '../styles/dynamicTheme.module.scss';
 import fullConfig from '../../theme.json'; 
 
 const rawSiteName = process.env.NEXT_PUBLIC_SITE_NAME || 'El Motorista';
-const siteName = rawSiteName.replace(/^["']|["']$/g, '');
+const siteName = rawSiteName.replace(/^["']|["']$/g, '').trim();
 
 export const config = (fullConfig as any)[siteName];
 export { themeStyles };
